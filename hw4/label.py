e906b1a0dfa1054b6677fe08e4604762e6f4f385
liangshuche <b04901019@ntu.edu.tw>
2017-11-30 16:29:55 +0800

    label

--- hw4/label.py
+++ hw4/label.py
@@ -9,9 +9,22 @@ from keras.models import load_model
 
 model=load_model(argv[1])
 x_train=np.load('data/x_train_nolabel.npy')
-result=model.predict(x_train,batch_size=256,verbose=1)
-print (result)
-'''
-labeled_data=0
+result=model.predict(x_train,batch_size=512,verbose=1)
+#print (result)
+
+x_train_semi=[]
+y_train_semi=[]
+
 for i in range(x_train.shape[0]):
-'''
+	if(result[i][0]>float(argv[2])):
+		x_train_semi.append(x_train[i])
+		y_train_semi.append([1,0])
+	if(result[i][1]>float(argv[2])):
+		x_train_semi.append(x_train[i])
+		y_train_semi.append([0,1])
+
+x_train_semi=np.array(x_train_semi)
+y_train_semi=np.array(y_train_semi)
+
+np.save('data/x_train_semi.npy',x_train_semi)
+np.save('data/y_train_semi.npy',y_train_semi)
