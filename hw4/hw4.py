93de77d3de8e7ec9c69e6a8b71e5c122e4dcaf5e
liangshuche <b04901019@ntu.edu.tw>
2017-11-30 15:49:16 +0800

    hw4.py

--- hw4/hw4.py
+++ hw4/hw4.py
@@ -36,7 +36,13 @@ def readlabeldata(path):
 	return data,label
 
 
-#def readnonlabeldata:
+def readnolabeldata(path):
+	print('Reading Non-Labeled Data...')
+	data=[]
+	with open(path) as f:
+		for line in f:
+			data.append(line)
+	return data
 
 def readtestdata(path):
 	print('Reading Test Data...')
@@ -51,6 +57,7 @@ def readtestdata(path):
 	return data		
 
 train_labeled_text,train_labeled_label=readlabeldata(train_label_path)
+train_nolabel=readnolabeldata(train_nolabel_path)
 test_text=readtestdata(test_path)
 
 text_target=list(set().union(train_labeled_text,test_text))
@@ -59,6 +66,7 @@ tokenizer=Tokenizer()
 tokenizer.fit_on_texts(text_target)
 
 x_train_labeled=tokenizer.texts_to_sequences(train_labeled_text)
+x_train_nolabel=tokenizer.texts_to_sequences(train_nolabel)
 x_test=tokenizer.texts_to_sequences(test_text)
 
 sequence_len=max(max(len(i) for i in x_train_labeled),max(len(j) for j in x_test))
@@ -66,10 +74,13 @@ vocab_size=len(tokenizer.word_index)+1
 print("Total Unique Words: %d, Max Sequence Length: %d" % (vocab_size,sequence_len))
 
 x_train_labeled=pad_sequences(x_train_labeled,maxlen=sequence_len, padding='post')
+x_train_nolabel=pad_sequences(x_train_nolabel,maxlen=sequence_len, padding='post')
 x_test=pad_sequences(x_test,maxlen=sequence_len, padding='post')
 
 
 x_train_labeled=np.array(x_train_labeled)
+x_train_nolabel=np.array(x_train_nolabel)
+print (x_train_nolabel)
 #print (train_labeled_label)
 y_train_labeled=np.array(train_labeled_label)
 #print (y_train_labeled)
